95a6879971307c4234bb56b236c58ae3a60396e1
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-01-29 18:34:04 +0530

    Update Stock Maximize.java

--- 24-01-2024/Stock Maximize.java
+++ 24-01-2024/Stock Maximize.java
@@ -7,6 +7,16 @@ public class MyClass {
             profit+=(maxsofar-a[i]);
         }
         return profit;
+
+      // int n=prices.length;
+      // int profit=0;
+      // for(int i=1;i<n;i++){
+      //     if(prices[i]>prices[i-1]){
+      //         profit+=(prices[i]-prices[i-1]);
+      //     }
+      // }  
+      // return profit;
+        
     }
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
