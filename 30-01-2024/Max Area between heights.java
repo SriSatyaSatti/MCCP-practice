9d74585b9abb035060c6d05a60c675d9cb1741d4
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-01-30 22:27:46 +0530

    Update Max Area between heights.java

--- 30-01-2024/Max Area between heights.java
+++ 30-01-2024/Max Area between heights.java
@@ -19,3 +19,23 @@ public class MyClass {
 	}
 
 }
+
+// Brute Force
+// import java.util.*;
+// public class MyClass{
+// 	public static void main(String[] args) {
+// 		Scanner sc=new Scanner(System.in);
+// 		int n=sc.nextInt();
+// 		int[] m=new int[n];
+// 		for(int i=0;i<n;i++) {
+// 			m[i]=sc.nextInt();
+// 		}
+// 		int max=0;
+// 		for(int i=0;i<n;i++) {
+// 			for(int j=i+1;j<n;j++)
+// 			max=Math.max(Math.min(m[i],m[j])*(j-i),max);
+// 		}
+// 		System.out.println(max);
+// 	}
+
+// }
