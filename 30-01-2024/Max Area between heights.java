e6bbe70c0b938777547076476eb5de0367ab2ea3
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-01-30 22:26:17 +0530

    Create Max Area between heights.java

---
+++ 30-01-2024/Max Area between heights.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MyClass {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] m=new int[n];
+		for(int i=0;i<n;i++) {
+			m[i]=sc.nextInt();
+		}
+		int i=0,j=n-1,max=0;
+		while(i<j) {
+			if(m[i]<m[j])
+		    	i++;
+			else
+				j--;
+			max=Math.max(Math.min(m[i],m[j])*(j-i),max);
+		}
+		System.out.println(max);
+	}
+
+}
