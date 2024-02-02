31cddd93779a47e2c40db7d4820da686ac30277c
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-02-02 21:33:00 +0530

    Create min sum of prod.java

---
+++ 31-01-2024/min sum of prod.java
@@ -0,0 +1,25 @@
+public class MyClass {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("length");
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		System.out.println("array");
+		for(int i=0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		int[] b=new int[n];
+		System.out.println("array");
+		for(int i=0;i<n;i++) {
+			b[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+		Arrays.sort(b);
+		int s=0;
+		for(int i=0;i<n;i++) {
+			s=s+a[i]*b[n-i-1];
+		}
+		System.out.println("minimum sum : "+s);
+	}
+
+}
