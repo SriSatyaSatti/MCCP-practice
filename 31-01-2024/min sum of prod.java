aa9ab96293750d7d1a9de47da41773e175090bd9
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-02-02 21:41:10 +0530

    Update min sum of prod.java

--- 31-01-2024/min sum of prod.java
+++ 31-01-2024/min sum of prod.java
@@ -2,15 +2,12 @@ import java.util.*;
 public class MyClass {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("length");
 		int n=sc.nextInt();
 		int[] a=new int[n];
-		System.out.println("array");
 		for(int i=0;i<n;i++) {
 			a[i]=sc.nextInt();
 		}
 		int[] b=new int[n];
-		System.out.println("array");
 		for(int i=0;i<n;i++) {
 			b[i]=sc.nextInt();
 		}
