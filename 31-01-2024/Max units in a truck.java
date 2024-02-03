a63a39c804b4e519a41e20805cb2e8fe70da7efb
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-02-03 19:43:13 +0530

    Update Max units in a truck.java

--- 31-01-2024/Max units in a truck.java
+++ 31-01-2024/Max units in a truck.java
@@ -1,14 +1,9 @@
-public class Deltoday {
-
+public class MyClass {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("length");
 		int n=sc.nextInt();
-		System.out.println("T");
 		int T=sc.nextInt();
 		int[][] a=new int[n][2];
-		System.out.println("array");
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<2;j++)
 			a[i][j]=sc.nextInt();
