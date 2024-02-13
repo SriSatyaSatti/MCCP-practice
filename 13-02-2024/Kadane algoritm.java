3fe8fe7295476098605c8842f622a7addf31d113
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-02-13 21:41:59 +0530

    Create Kadane algoritm.java

---
+++ 13-02-2024/Kadane algoritm.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MyClass {
+    static int operations(int a[],int n){
+        int max=a[0];
+        int cmax=a[0];
+        for(int i=0;i<n;i++){
+            cmax=Math.max(a[i],cmax+a[i]);
+            max=Math.max(cmax,max);
+        }
+        return max;
+    }
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.print(operations(a,n));
+    }
+}
