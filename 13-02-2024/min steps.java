cf336252ae7549fa916efd9bea6a4dc9a5b7add3
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-02-19 23:36:55 +0530

    Update min steps.java

--- 13-02-2024/min steps.java
+++ 13-02-2024/min steps.java
@@ -26,3 +26,13 @@ public class MyClass {
         System.out.print(operations(n));
     }
 }
+
+
+
+
+
+    if (n <= 1)
+        return n;
+    if (dp.count(n) == 0)
+        dp[n] = 1 + min(n % 2 + minDays(n / 2), n % 3 + minDays(n / 3));
+    return dp[n];
