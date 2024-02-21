16574da69747224c82e296f43dcb1703f717d2c1
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-02-21 21:43:39 +0530

    Create Rotate.java

---
+++ 21-02-2024/Rotate.java
@@ -0,0 +1,50 @@
+import java.util.*;
+	class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data=data;
+			next=null;
+		}
+	}
+public class MyClass {
+	public static Node head=null;
+    static Node insert(int ele,Node head) {
+		Node temp=new Node(ele);
+		Node p=head;
+		if(head==null)
+			head=temp;
+		else {
+		while(p.next!=null) {
+			p=p.next;
+		}
+		p.next=temp;
+		}
+		return head;
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) 
+			head=insert(sc.nextInt(),head);
+		moveLast(head);
+	}
+    static void moveLast(Node head) {
+		Node head2=head;
+		while(head2.next.next!=null)
+			head2=head2.next;
+		Node last=head2.next;
+		head2.next=null;
+		last.next=head;
+		head=last;
+		display(head);
+	}
+	static void display(Node head) {
+		Node temp=head;
+		while(temp!=null) {
+			System.out.print(temp.data+" ");
+			temp=temp.next;
+		}
+		System.out.println();
+	}
+}
