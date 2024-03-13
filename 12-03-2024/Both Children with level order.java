293e2a364fd852392b7d78121c0caed89422a65d
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-03-13 09:00:34 +0530

    Create Both Children with level order.java

---
+++ 12-03-2024/Both Children with level order.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class twochilds_level_order {
+	public static class Node
+	{
+		int data;
+		Node left,right;
+		Node(int data)
+		{
+			this.data=data;
+			left=null;
+			right=null;
+		}
+	}
+	static Node root=null;
+	public static Node insert(Node root,int key)
+	{
+		if(root==null)
+		{
+			Node t=new Node(key);
+			return t;
+		}
+		else if(root.data<key)
+		{
+			root.right=insert(root.right,key);
+		}
+		else if(root.data>key)
+		{
+			root.left=insert(root.left,key);
+		}
+		return root;
+	}
+	public static void nonleaf(Node root)
+	{
+		if(root==null)
+		{
+			return ;
+		}
+		Queue<Node> q=new LinkedList<>();
+		q.add(root);
+		while(!q.isEmpty())
+		{
+			Node temp=q.poll();
+			if(temp.left!=null && temp.right!=null)
+			{
+				System.out.print(temp.data+" ");
+			}
+			if(temp.left!=null)
+			{
+				q.add(temp.left);
+			}
+			if(temp.right!=null)
+			{
+				q.add(temp.right);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		root=insert(root,20);
+		root=insert(root,10);
+		root=insert(root,40);
+		root=insert(root,5);
+		root=insert(root,12);
+		root=insert(root,30);
+		root=insert(root,60);
+		nonleaf(root);
+	}
+}
