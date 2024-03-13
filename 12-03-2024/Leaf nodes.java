39970458879f438dadb73d6a9a6a2b35d91062b9
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-03-13 09:11:08 +0530

    Create Leaf nodes.java

---
+++ 12-03-2024/Leaf nodes.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class leaf_nodes {
+	public static class Node
+	{
+		int data;
+		Node left,right;
+		Node(int data)
+		{
+			this.data=data;
+			left=null;
+			right=null;
+		}
+	}
+	static Node root=null;
+	public static Node insert(Node root,int key)
+	{
+		if(root==null)
+		{
+			Node t=new Node(key);
+			return t;
+		}
+		else if(root.data<key)
+		{
+			root.right=insert(root.right,key);
+		}
+		else if(root.data>key)
+		{
+			root.left=insert(root.left,key);
+		}
+		return root;
+	}
+	public static void leaf(Node root)
+	{
+		if(root!=null)
+		{
+			if(root.left==null && root.right==null)
+			{
+				System.out.print(root.data+" ");
+			}
+			leaf(root.left);
+			leaf(root.right);
+		}
+	}
+	public static void main(String[] args) {
+		root=insert(root,20);
+		root=insert(root,10);
+		root=insert(root,40);
+		root=insert(root,5);
+		root=insert(root,12);
+		root=insert(root,30);
+		root=insert(root,60);
+		leaf(root);
+	}
+}
