7a54a150b461b1c1be1bcc8783a240979da2fcf3
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-03-13 09:01:57 +0530

    Create Reverse level order.java

---
+++ 12-03-2024/Reverse level order.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class reverse_levelorder_stack {
+	public static class Node
+	{
+		int data;
+		Node left,right;
+		Node(int data)
+		{
+			this.data=data;
+			left=null;
+			right=null;
+		}
+	}
+	static Node root=null;
+	public static Node insert(Node root,int key)
+	{
+		if(root==null)
+		{
+			Node t=new Node(key);
+			return t;
+		}
+		else if(root.data<key)
+		{
+			root.right=insert(root.right,key);
+		}
+		else if(root.data>key)
+		{
+			root.left=insert(root.left,key);
+		}
+		return root;
+	}
+	public static void reverse_levelorder(Node root)
+	{
+		if(root==null)
+		{
+			return ;
+		}
+		Stack<Integer> s=new Stack<>();
+		Queue<Node> q=new LinkedList<>();
+		q.add(root);
+		while(!q.isEmpty())
+		{
+			root=q.poll();
+			s.push(root.data);
+			if(root.right!=null)
+			{
+				q.add(root.right);
+			}
+			if(root.left!=null)
+			{
+				q.add(root.left);
+			}
+		}
+		while(!s.isEmpty())
+		{
+			System.out.print(s.peek()+" ");
+			s.pop();
+		}
+	}
+	public static void main(String[] args) {
+		root=insert(root,20);
+		root=insert(root,10);
+		root=insert(root,40);
+		root=insert(root,5);
+		root=insert(root,12);
+		root=insert(root,30);
+		root=insert(root,60);
+		reverse_levelorder(root);
+	}
+}
