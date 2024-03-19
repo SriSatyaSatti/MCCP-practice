d46191615499193d41d8784feffe21a9dd242b4f
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-03-19 12:38:37 +0530

    Create Min Max in BST.java

---
+++ 19-03-2024/Min Max in BST.java
@@ -0,0 +1,56 @@
+public class MyClass {
+	public static class Node
+	{
+		int data;
+		Node left,right;
+		Node(int data)
+		{
+			this.data=data;
+			left=null;
+			right=null;
+		}
+	}
+	static Node root=null;
+	public static Node insert(Node root,int key)
+	{
+		if(root==null)
+		{
+			Node t=new Node(key);
+			return t;
+		}
+		else if(root.data<key)
+		{
+			root.right=insert(root.right,key);
+		}
+		else if(root.data>key)
+		{
+			root.left=insert(root.left,key);
+		}
+		return root;
+	}
+	
+	static int min(Node root)
+	{
+	    if(root.left==null)
+	    return root.data;
+	    return min(root.left);
+	}
+	static int max(Node root)
+	{
+	    if(root.right==null)
+	    return root.data;
+	    return min(root.right);
+	}
+	public static void main(String[] args) {
+		root=insert(root,20);
+		root=insert(root,10);
+		root=insert(root,40);
+		root=insert(root,5);
+		root=insert(root,12);
+		root=insert(root,30);
+		root=insert(root,60);
+		System.out.print(min(root)+" ");
+		System.out.print(max(root));
+	}
+
+}
