3ab80fd35d82ecddc3b1dcc3f1f019167ce9c6c2
Sri Satya Satti <102864137+SriSatyaSatti@users.noreply.github.com>
2024-03-20 16:14:28 +0530

    Create BFS using list iteration.java

---
+++ 20-03-2024/BFS using list iteration.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class HelloWorld {
+    static void graph(Scanner sc, int n){
+    HashMap<Integer,ArrayList<Integer>> h=new HashMap<>();
+    int u,v,ch=1;
+    int r=sc.nextInt();
+    while(ch==1){
+        System.out.print("Enter edge with nodes between 0 and "+n+": ");
+        u=sc.nextInt();
+        v=sc.nextInt();
+        ArrayList<Integer> a=new ArrayList<>();
+        if(!h.containsKey(u)){
+            a.add(v);
+            h.put(u,a);
+        }else{
+            a=h.get(u);
+            a.add(v);
+            h.put(u,a);
+        }
+        System.out.print("Enter 1 to continue");
+        ch=sc.nextInt();
+    }
+    DFS(h,n,r);
+}
+    static void BFS(HashMap<Integer,ArrayList<Integer>> h,int n,int v){
+        Queue<Integer> q=new LinkedList<>();
+        s.add(v);
+        int visited[]=new int[n];
+        while(!s.isEmpty()){
+            int k=s.poll();
+            if(visited[k]==0)
+            System.out.print(k+" ");
+            visited[k]=1;
+        for(Integer i:h.get(v)){
+            if(visited[i]==0)
+                s.add(i);
+            }
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        graph(sc,n);
+    }
+}
